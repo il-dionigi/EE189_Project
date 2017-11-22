03a7fd8f8b91554dc26431a2feefd2f9b45a3d07
il-dionigi <brian.dionigi@gmail.com>
2017-11-22 15:40:31 -0800

    Better verifier
    Uses a helper function and verbosity can be switched on and off

--- ProjectFiles/VerifyPeriods.m
+++ ProjectFiles/VerifyPeriods.m
@@ -8,145 +8,95 @@ load('Signals/Noisy/LowNoise/PeriodicInCompleteFewLowNoise.mat');
 load('Signals/Noisy/LowNoise/PeriodicCompleteManyLowNoise.mat');
 load('Signals/Noisy/LowNoise/PeriodicCompleteFewLowNoise.mat');
 
-verbose = 0;
+verbose = 1; % Change this to limit or delimit the ammount of info returned
 
-errorTotal = 0;
+correctTotal = 0;
+percentErrorAverage = [0, 0];
 
-errors = 0;
-for i = 1:1:8
-    periodEstimated = PeriodDetector(Y_PeriodicCompleteFewHighNoise{i});
-    periodActual = Answers_PeriodicCompleteFewHighNoise(i);
-    if abs(periodEstimated-periodActual)>=periodActual*0.2
-       errors = errors + 1;
-       if verbose
-          fprintf("The index of the incorrect vector was %d\n", i);
-       end
+disp("Verifying PeriodicCompleteFewHighNoise...");
+[correct, percentIncorrect] = verify(Y_PeriodicCompleteFewHighNoise, ...
+    Answers_PeriodicCompleteFewHighNoise, verbose);
+correctTotal = correctTotal + correct;
+percentErrorAverage(1) = ((percentErrorAverage(1) * percentErrorAverage(2)) ...
+    + percentIncorrect) / (percentErrorAverage(2) + 1);
+percentErrorAverage(2) = percentErrorAverage(2) + 1;
+
+disp("Verifying PeriodicCompleteFewLowNoise...");
+[correct, percentIncorrect] = verify(Y_PeriodicCompleteFewLowNoise, ...
+    Answers_PeriodicCompleteFewLowNoise, verbose);
+correctTotal = correctTotal + correct;
+percentErrorAverage(1) = ((percentErrorAverage(1) * percentErrorAverage(2)) ...
+    + percentIncorrect) / (percentErrorAverage(2) + 1);
+percentErrorAverage(2) = percentErrorAverage(2) + 1;
+
+disp("Verifying PeriodicCompleteManyHighNoise...");
+[correct, percentIncorrect] = verify(Y_PeriodicCompleteManyHighNoise, ...
+    Answers_PeriodicCompleteManyHighNoise, verbose);
+correctTotal = correctTotal + correct;
+percentErrorAverage(1) = ((percentErrorAverage(1) * percentErrorAverage(2)) ...
+    + percentIncorrect) / (percentErrorAverage(2) + 1);
+percentErrorAverage(2) = percentErrorAverage(2) + 1;
+
+disp("Verifying PeriodicCompleteManyLowNoise...");
+[correct, percentIncorrect] = verify(Y_PeriodicCompleteManyLowNoise, ...
+    Answers_PeriodicCompleteManyLowNoise, verbose);
+correctTotal = correctTotal + correct;
+percentErrorAverage(1) = ((percentErrorAverage(1) * percentErrorAverage(2)) ...
+    + percentIncorrect) / (percentErrorAverage(2) + 1);
+percentErrorAverage(2) = percentErrorAverage(2) + 1;
+
+disp("Verifying PeriodicInCompleteFewHighNoise...");
+[correct, percentIncorrect] = verify(Y_PeriodicInCompleteFewHighNoise, ...
+    Answers_PeriodicInCompleteFewHighNoise, verbose);
+correctTotal = correctTotal + correct;
+percentErrorAverage(1) = ((percentErrorAverage(1) * percentErrorAverage(2)) ...
+    + percentIncorrect) / (percentErrorAverage(2) + 1);
+percentErrorAverage(2) = percentErrorAverage(2) + 1;
+
+disp("Verifying PeriodicInCompleteFewLowNoise...");
+[correct, percentIncorrect] = verify(Y_PeriodicInCompleteFewLowNoise, ...
+    Answers_PeriodicInCompleteFewLowNoise, verbose);
+correctTotal = correctTotal + correct;
+percentErrorAverage(1) = ((percentErrorAverage(1) * percentErrorAverage(2)) ...
+    + percentIncorrect) / (percentErrorAverage(2) + 1);
+percentErrorAverage(2) = percentErrorAverage(2) + 1;
+
+disp("Verifying PeriodicInCompleteManyHighNoise...");
+[correct, percentIncorrect] = verify(Y_PeriodicInCompleteManyHighNoise, ...
+    Answers_PeriodicInCompleteManyHighNoise, verbose);
+correctTotal = correctTotal + correct;
+percentErrorAverage(1) = ((percentErrorAverage(1) * percentErrorAverage(2)) ...
+    + percentIncorrect) / (percentErrorAverage(2) + 1);
+percentErrorAverage(2) = percentErrorAverage(2) + 1;
+
+disp("Verifying PeriodicInCompleteManyLowNoise...");
+[correct, percentIncorrect] = verify(Y_PeriodicInCompleteManyLowNoise, ...
+    Answers_PeriodicInCompleteManyLowNoise, verbose);
+correctTotal = correctTotal + correct;
+percentErrorAverage(1) = ((percentErrorAverage(1) * percentErrorAverage(2)) ...
+    + percentIncorrect) / (percentErrorAverage(2) + 1);
+percentErrorAverage(2) = percentErrorAverage(2) + 1;
+
+fprintf("Number correct in total: %d\n", correctTotal);
+fprintf("This equates to %.2f percent correct\n", (correctTotal / 64) * 100);
+fprintf("with an average percent error of %.2f\n", percentErrorAverage(1));
+
+function [ numCorrect, percentErrorAverage ] = verify(signals, answers, verbose)
+    totalNumber = length(signals);
+    percentIncorrect = zeros(totalNumber, 1);
+    for i = 1:1:totalNumber
+        percentIncorrect(i) = (abs(answers(i)-PeriodDetector(signals{i}))/answers(i))*100;
+    end
+    numCorrect = sum(percentIncorrect <= 0.2);
+    percentErrorAverage = mean(percentIncorrect);
+    indices = find(percentIncorrect <= 0.2);
+    if verbose
+        fprintf("Number correct out of %d was %d\n", totalNumber, numCorrect);
+        fprintf("Average percent error was %.2f\n", percentErrorAverage);
+        if numCorrect ~= totalNumber
+            fprintf("Indices of incorrect periods: ");
+            fprintf("%d ", indices);
+            fprintf("\n\n");
+        end
     end
 end
-if errors
-    fprintf("Number wrong in PeriodicCompleteFewHighNoise:\n%d/8\n", errors);
-else
-   fprintf("None wrong in PeriodicCompleteFewHighNoise!\n");
-end
-errorTotal = errorTotal + errors;
-errors = 0;
-for i = 1:1:8
-    periodEstimated = PeriodDetector(Y_PeriodicCompleteFewLowNoise{i});
-    periodActual = Answers_PeriodicCompleteFewLowNoise(i);
-    if abs(periodEstimated-periodActual)>=periodActual*0.2
-       errors = errors + 1;
-       if verbose
-          fprintf("The index of the incorrect vector was %d\n", i);
-       end
-    end
-end
-if errors
-    fprintf("Number wrong in PeriodicCompleteFewLowNoise:\n%d/8\n", errors);
-else
-   fprintf("None wrong in PeriodicCompleteFewLowNoise!\n");
-end
-errorTotal = errorTotal + errors;
-errors = 0;
-for i = 1:1:8
-    periodEstimated = PeriodDetector(Y_PeriodicCompleteManyHighNoise{i});
-    periodActual = Answers_PeriodicCompleteManyHighNoise(i);
-    if abs(periodEstimated-periodActual)>=periodActual*0.2
-       errors = errors + 1;
-       if verbose
-          fprintf("The index of the incorrect vector was %d\n", i);
-       end
-    end
-end
-if errors
-    fprintf("Number wrong in PeriodicCompleteManyHighNoise:\n%d/8\n", errors);
-else
-   fprintf("None wrong in PeriodicCompleteManyHighNoise!\n");
-end
-errorTotal = errorTotal + errors;
-errors = 0;
-for i = 1:1:8
-    periodEstimated = PeriodDetector(Y_PeriodicCompleteManyLowNoise{i});
-    periodActual = Answers_PeriodicCompleteManyLowNoise(i);
-    if abs(periodEstimated-periodActual)>=periodActual*0.2
-       errors = errors + 1;
-       if verbose
-          fprintf("The index of the incorrect vector was %d\n", i);
-       end
-    end
-end
-if errors
-    fprintf("Number wrong in PeriodicCompleteManyLowNoise:\n%d/8\n", errors);
-else
-   fprintf("None wrong in PeriodicCompleteManyLowNoise!\n");
-end
-errorTotal = errorTotal + errors;
-errors = 0;
-for i = 1:1:8
-    periodEstimated = PeriodDetector(Y_PeriodicInCompleteFewHighNoise{i});
-    periodActual = Answers_PeriodicInCompleteFewHighNoise(i);
-    if abs(periodEstimated-periodActual)>=periodActual*0.2
-       errors = errors + 1;
-       if verbose
-          fprintf("The index of the incorrect vector was %d\n", i);
-       end
-    end
-end
-if errors
-    fprintf("Number wrong in PeriodicInCompleteFewHighNoise:\n%d/8\n", errors);
-else
-   fprintf("None wrong in PeriodicInCompleteFewHighNoise!\n");
-end
-errorTotal = errorTotal + errors;
-errors = 0;
-for i = 1:1:8
-    periodEstimated = PeriodDetector(Y_PeriodicInCompleteFewLowNoise{i});
-    periodActual = Answers_PeriodicInCompleteFewLowNoise(i);
-    if abs(periodEstimated-periodActual)>=periodActual*0.2
-       errors = errors + 1;
-       if verbose
-          fprintf("The index of the incorrect vector was %d\n", i);
-       end
-    end
-end
-if errors
-fprintf("Number wrong in PeriodicInCompleteFewLowNoise:\n%d/8\n", errors);
-else
-   fprintf("None wrong in PeriodicInCompleteFewLowNoise!\n");
-end
-errorTotal = errorTotal + errors;
-errors = 0;
-for i = 1:1:8
-    periodEstimated = PeriodDetector(Y_PeriodicInCompleteManyHighNoise{i});
-    periodActual = Answers_PeriodicInCompleteManyHighNoise(i);
-    if abs(periodEstimated-periodActual)>=periodActual*0.2
-       errors = errors + 1;
-       if verbose
-          fprintf("The index of the incorrect vector was %d\n", i);
-       end
-    end
-end
-if errors
-fprintf("Number wrong in PeriodicInCompleteManyHighNoise:\n%d/8\n", errors);
-else
-   fprintf("None wrong in PeriodicInCompleteManyHighNoise!\n");
-end
-errorTotal = errorTotal + errors;
-errors = 0;
-for i = 1:1:8
-    periodEstimated = PeriodDetector(Y_PeriodicInCompleteManyLowNoise{i});
-    periodActual = Answers_PeriodicInCompleteManyLowNoise(i);
-    if abs(periodEstimated-periodActual)>=periodActual*0.2
-       errors = errors + 1;
-       if verbose
-          fprintf("The index of the incorrect vector was %d\n", i);
-       end
-    end
-end
-if errors
-   fprintf("Number wrong in PeriodicInCompleteManyLowNoise:\n%d/8\n", errors);
-else
-   fprintf("None wrong in PeriodicInCompleteManyLowNoise!\n");
-end
-errorTotal = errorTotal + errors;
-fprintf("Number wrong in total:\n%d\n", errorTotal);
-fprintf("This equates to %.2f percent wrong\n", (errorTotal / 64) * 100);
