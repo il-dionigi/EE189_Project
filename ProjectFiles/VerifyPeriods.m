af1edad298cf3d7ca1cc29712ddde73cacd1db03
il-dionigi <brian.dionigi@gmail.com>
2017-11-21 18:30:30 -0800

    Period Detector & Verifier
    Empty functions

---
+++ ProjectFiles/VerifyPeriods.m
@@ -0,0 +1,10 @@
+load('Signals/RealSignals/RealSignals.mat');
+load('Signals/Noisy/HighNoise/PeriodicInCompleteManyHighNoise.mat');
+load('Signals/Noisy/HighNoise/PeriodicInCompleteFewHighNoise.mat');
+load('Signals/Noisy/HighNoise/PeriodicCompleteManyHighNoise.mat');
+load('Signals/Noisy/HighNoise/PeriodicCompleteFewHighNoise.mat');
+load('Signals/Noisy/LowNoise/PeriodicInCompleteManyLowNoise.mat');
+load('Signals/Noisy/LowNoise/PeriodicInCompleteFewLowNoise.mat');
+load('Signals/Noisy/LowNoise/PeriodicCompleteManyLowNoise.mat');
+load('Signals/Noisy/LowNoise/PeriodicCompleteFewLowNoise.mat');
+
