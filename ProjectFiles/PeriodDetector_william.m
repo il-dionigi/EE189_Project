da25bc1ca2c576163d8e765ac221b347a6efa9ca
williamow <wow@dcn.org>
2017-11-28 20:07:14 -0800

    Revert "Revert "A period detector method and bug fix""
    This reverts commit 400f4938da1097e04c7c8ff343f465a06bd40cf5.

---
+++ ProjectFiles/PeriodDetector_william.m
@@ -0,0 +1,50 @@
+function [ bestPeriod ] = PeriodDetector_william( rawData )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+    useSFFT = 0;
+    bestPeriod = numel(rawData);
+    rawData = rawData - mean(rawData(:));
+    
+    if useSFFT
+        k = 4;
+        l = best_length(length(rawData), k);
+        try
+            [transform, f] = SFFT(rawData(1:l), k);
+        catch
+            [transform, f] = SFFT(rawData(1:l), k);
+        end
+        transform = abs(transform);
+    else
+        l = length(rawData);
+        transform = fft(rawData(1:l));
+        transform = abs(transform);
+        mx = max(transform);
+        transform = transform.*(transform > 0.25*mx);
+        [~, f] = findpeaks(transform);
+    end
+
+    %simple hypothesis for what the first harmonic of ther period is
+    if numel(f) > 0
+        f = f-1;
+        x = f(1);
+    else
+        return
+    end
+    if x == 1
+        x = 2;
+    end
+    
+    %iteratively refine what we think the fundamental harmonic is
+    weight = zeros(size(f));
+    harmonic = zeros(size(f));
+    for i = 1:numel(f)
+        weight(i) = transform(f(i)+1);
+        for j = 1:i
+            harmonic(j) = single(uint8(f(j)/x));
+        end
+        %a linear regression function
+        x = lscov(transpose(harmonic(1:i)), transpose(f(1:i)), transpose(weight(1:i)));
+    end
+    
+    bestPeriod = l/x;
+end
